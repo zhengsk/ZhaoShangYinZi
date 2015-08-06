405f0321820a4b6280859f14cd002c980cadfa5b
城管大队长 <zsk526@qq.com>
2015-08-07 01:23:13 +0800

    edit

--- assets/js/common.js
+++ assets/js/common.js
@@ -1,4 +1,11 @@
 
+// 页面导航地位
+;(function setNavLocation(){
+	var locationId = "#sadfas";
+	$(locationId).addClass('active')
+})();
+
+
 // 获取地址栏参数
 function getUrlParam(name) {
     var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)");
@@ -10,6 +17,36 @@ function getUrlParam(name) {
 // 隐藏不需要的内容
 function detailOnlyShow (param) {
     if(getUrlParam(param) === "1"){
-        document.getElementsByTagName('body')[0].className = "only-show-article";
+        $(document.getElementsByTagName('body')[0]).addClass('only-show-article');
+    }
+}
+
+// 详情页显示广告
+function showBottomAd (query, url) {
+    if(getUrlParam(query) === "1"){
+        $(document.getElementsByTagName('body')[0]).addClass('show-bottom-ad');
+
+        $.ajax({
+        	url: url,
+        	type: 'get',
+        	dataType: 'json'
+        })
+        .done(function(datas) {
+        	console.log("success");
+        	if(datas.status === 200){
+        		var adDatas = datas.results;
+        		$.each(adDatas, function(i, item){
+        			var ad = $('<img src="'+ item.image +'" class="width-100">');
+        			if(item.param && item.param.url){
+        				var link = $('<a href="'+ item.param.url +'"></a>');
+        				ad = link.append(ad);
+        			}
+        			$('#pageBottomAd').append(ad);
+        		});
+        	}else{
+        		console.warn('加载底部广告失败！');
+        	}
+        });
+        
     }
 }
